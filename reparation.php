b949d30477e07594566b456025b0c53f777d0f03
GaelleLC <57903519+GaelleLC@users.noreply.github.com>
2019-11-18 17:11:10 +0100

    rep pas fini

--- reparation.php
+++ reparation.php
@@ -11,7 +11,68 @@ include 'headerfooter.php';
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body style = "background-color: grey;">
-        
+        <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+            <h1 class="display-4 text-light">Réparations</h1>
+            <p class="lead text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe libero ad rerum suscipit perferendis rem, explicabo commodi error repellendus qui harum quae odio nemo nulla! Deleniti repellat ad quo ipsa. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque iure ducimus sequi autem officia ea rem adipisci distinctio amet eius tenetur soluta, harum nam praesentium totam deserunt eaque sed corrupti! </p> 
+        </div>
+        <div class="container">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="card">
+                        <h5 class="card-header">Prestation 1</h5>
+                        <div class="card-body">
+                            <h5 class="card-title">Titre presta</h5>
+                            <p class="card-text">Description rapide prestation</p>
+                            <a href="contact.php" class="btn btn-primary">Réserver</a>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <h5 class="card-header">Prestation 1</h5>
+                        <div class="card-body">
+                            <h5 class="card-title">Titre presta</h5>
+                            <p class="card-text">Description rapide prestation</p>
+                            <a href="contact.php" class="btn btn-primary">Réserver</a>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <h5 class="card-header">Prestation 1</h5>
+                        <div class="card-body">
+                            <h5 class="card-title">Titre presta</h5>
+                            <p class="card-text">Description rapide prestation</p>
+                            <a href="contact.php" class="btn btn-primary">Réserver</a>
+                        </div>
+                    </div>
+                    
+                    
+                    
+                    <!-- <div class="d-flex flex-column">
+                        <br><h3 class="text-left w-100 text-light">Prestation 1</h3>
+                        <div class="d-flex flex-row">
+                        <p class="text-left w-100 text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum saepe ut quaerat quasi corrupti facere deleniti officia. Facere pariatur laudantium, molestias nihil repudiandae distinctio. Rerum qui itaque in vitae eaque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, non libero. Architecto ullam quo amet ex id, ut unde pariatur minima nesciunt omnis soluta dolor, fugiat quisquam itaque? Rerum, officia?</p>
+                        <p class="text-right w-100 text-light">Prix €</p>
+                        </div>
+
+                    <div class="d-flex flex-column">
+                        <br><h3 class="text-left w-100 text-light">Prestation 2</h3>
+                        <div class="d-flex flex-row">
+                        <p class="text-left w-100 text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum saepe ut quaerat quasi corrupti facere deleniti officia. Facere pariatur laudantium, molestias nihil repudiandae distinctio. Rerum qui itaque in vitae eaque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, non libero. Architecto ullam quo amet ex id, ut unde pariatur minima nesciunt omnis soluta dolor, fugiat quisquam itaque? Rerum, officia?</p>
+                        <p class="text-right w-100 text-light">Prix €</p>
+                        </div>
+
+                    <div class="d-flex flex-column">
+                        <br><h3 class="text-left w-100 text-light">Prestation 3</h3>
+                        <div class="d-flex flex-row">
+                        <p class="text-left w-100 text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum saepe ut quaerat quasi corrupti facere deleniti officia. Facere pariatur laudantium, molestias nihil repudiandae distinctio. Rerum qui itaque in vitae eaque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, non libero. Architecto ullam quo amet ex id, ut unde pariatur minima nesciunt omnis soluta dolor, fugiat quisquam itaque? Rerum, officia?</p>
+                        <p class="text-right w-100 text-light">Prix €</p>
+                        </div> -->
+
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
 
 
 
